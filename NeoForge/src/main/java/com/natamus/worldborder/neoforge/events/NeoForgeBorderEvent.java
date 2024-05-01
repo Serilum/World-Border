d8a638455579fd6dc321282841497cc3128d64cf
Rick South <rick@natamus.com>
2024-05-01 16:15:07 +0200

    World Border source code for 1.20.6.

---
+++ NeoForge/src/main/java/com/natamus/worldborder/neoforge/events/NeoForgeBorderEvent.java
@@ -0,0 +1,24 @@
+package com.natamus.worldborder.neoforge.events;
+
+import com.natamus.worldborder.events.BorderEvent;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.level.Level;
+import net.neoforged.neoforge.event.tick.PlayerTickEvent;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.EventBusSubscriber;
+
+@EventBusSubscriber
+public class NeoForgeBorderEvent {
+	@SubscribeEvent
+	public static void onPlayerTick(PlayerTickEvent.Pre e) {
+		Player player = e.getEntity();
+		Level level = player.level();
+		if (level.isClientSide) {
+			return;
+		}
+
+		BorderEvent.onPlayerTick((ServerLevel)level, (ServerPlayer)player);
+	}
+}

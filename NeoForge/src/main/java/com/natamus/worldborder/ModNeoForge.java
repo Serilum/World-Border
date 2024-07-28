f55437e3f045a2346a5d6fed923b9028248417bf
Rick South <rick@natamus.com>
2024-07-28 02:34:51 +0200

    (v4.7) Added Bundle support. The mod can now be enabled/disabled via bundle configs. [+1]

--- NeoForge/src/main/java/com/natamus/worldborder/ModNeoForge.java
+++ NeoForge/src/main/java/com/natamus/worldborder/ModNeoForge.java
@@ -1,6 +1,7 @@
 package com.natamus.worldborder;
 
 import com.natamus.collective.check.RegisterMod;
+import com.natamus.collective.check.ShouldLoadCheck;
 import com.natamus.worldborder.neoforge.config.IntegrateNeoForgeConfig;
 import com.natamus.worldborder.neoforge.events.NeoForgeBorderEvent;
 import com.natamus.worldborder.util.Reference;
@@ -15,6 +16,10 @@ import net.neoforged.fml.javafmlmod.FMLJavaModLoadingContext;
 public class ModNeoForge {
 	
 	public ModNeoForge(IEventBus modEventBus) {
+		if (!ShouldLoadCheck.shouldLoad(Reference.MOD_ID)) {
+			return;
+		}
+
 		modEventBus.addListener(this::loadComplete);
 
 		setGlobalConstants();

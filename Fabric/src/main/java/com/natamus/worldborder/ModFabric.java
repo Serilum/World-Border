8f633aa86d3707bde5168f27e1fb697dbd5d325c
Rick South <rick@natamus.com>
2024-07-28 02:34:55 +0200

    (v4.7) Added Bundle support. The mod can now be enabled/disabled via bundle configs. [+1]

--- Fabric/src/main/java/com/natamus/worldborder/ModFabric.java
+++ Fabric/src/main/java/com/natamus/worldborder/ModFabric.java
@@ -1,6 +1,7 @@
 package com.natamus.worldborder;
 
 import com.natamus.collective.check.RegisterMod;
+import com.natamus.collective.check.ShouldLoadCheck;
 import com.natamus.collective.fabric.callbacks.CollectivePlayerEvents;
 import com.natamus.worldborder.events.BorderEvent;
 import com.natamus.worldborder.util.Reference;
@@ -12,6 +13,10 @@ public class ModFabric implements ModInitializer {
 	
 	@Override
 	public void onInitialize() {
+		if (!ShouldLoadCheck.shouldLoad(Reference.MOD_ID)) {
+			return;
+		}
+
 		setGlobalConstants();
 		ModCommon.init();
 
--- NeoForge/src/main/java/com/natamus/worldborder/ModNeoForge.java
+++ NeoForge/src/main/java/com/natamus/worldborder/ModNeoForge.java
@@ -1,6 +1,7 @@
 package com.natamus.worldborder;
 
 import com.natamus.collective.check.RegisterMod;
+import com.natamus.collective.check.ShouldLoadCheck;
 import com.natamus.worldborder.neoforge.config.IntegrateNeoForgeConfig;
 import com.natamus.worldborder.neoforge.events.NeoForgeBorderEvent;
 import com.natamus.worldborder.util.Reference;
@@ -14,6 +15,10 @@ import net.neoforged.fml.event.lifecycle.FMLLoadCompleteEvent;
 public class ModNeoForge {
 	
 	public ModNeoForge(IEventBus modEventBus) {
+		if (!ShouldLoadCheck.shouldLoad(Reference.MOD_ID)) {
+			return;
+		}
+
 		modEventBus.addListener(this::loadComplete);
 
 		setGlobalConstants();

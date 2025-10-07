8d1bf9b11b5465675ce38dbb5934c8c77b90f6f6
Rick South <rick@natamus.com>
2025-10-08 00:47:11 +0200

    World Border source code for 1.21.10.

---
+++ Fabric/src/main/java/com/natamus/worldborder/ModFabric.java
@@ -0,0 +1,37 @@
+package com.natamus.worldborder;
+
+import com.natamus.collective.check.RegisterMod;
+import com.natamus.collective.check.ShouldLoadCheck;
+import com.natamus.collective.fabric.callbacks.CollectivePlayerEvents;
+import com.natamus.worldborder.events.BorderEvent;
+import com.natamus.worldborder.util.Reference;
+import net.fabricmc.api.ModInitializer;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.server.level.ServerPlayer;
+
+public class ModFabric implements ModInitializer {
+	
+	@Override
+	public void onInitialize() {
+		if (!ShouldLoadCheck.shouldLoad(Reference.MOD_ID)) {
+			return;
+		}
+
+		setGlobalConstants();
+		ModCommon.init();
+
+		loadEvents();
+
+		RegisterMod.register(Reference.NAME, Reference.MOD_ID, Reference.VERSION, Reference.ACCEPTED_VERSIONS);
+	}
+
+	private void loadEvents() {
+		CollectivePlayerEvents.PLAYER_TICK.register((ServerLevel world, ServerPlayer player) -> {
+			BorderEvent.onPlayerTick(world, player);
+		});
+	}
+
+	private static void setGlobalConstants() {
+
+	}
+}

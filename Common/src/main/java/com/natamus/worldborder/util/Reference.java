ad564eeb99873e40187d619f9d65f8d941253dac
Rick South <rick@natamus.com>
2026-06-18 12:32:59 +0200

    World Border source code for 26.2.0.

--- Common/src/main/java/com/natamus/worldborder/util/Reference.java
+++ Common/src/main/java/com/natamus/worldborder/util/Reference.java
@@ -4,5 +4,5 @@ public class Reference {
 	public static final String MOD_ID = "worldborder";
 	public static final String NAME = "World Border";
 	public static final String VERSION = "4.9";
-	public static final String ACCEPTED_VERSIONS = "[26.1.2]";
+	public static final String ACCEPTED_VERSIONS = "[26.2.0]";
 }
